eb6ff3cdba2e9a5bbadb08430261235d7071f449
Bade om <71856738+ombade@users.noreply.github.com>
2023-02-20 22:46:24 +0530

    first Commit . base level

---
+++ app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.unit_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        editText = findViewById(R.id.number);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                float gm = kg * 1000;
+                textView.setText("The value in grams is :"+gm);
+
+
+
+
+            }
+        });
+    }
+}
